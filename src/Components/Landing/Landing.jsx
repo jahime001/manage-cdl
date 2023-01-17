866e2a77d61ce0d81fcec09f1c6a0d344407e773
jahime001 <jahimecamea@gmail.com>
2023-01-17 00:02:22 -0500

    made some changes to section one of the home page

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -1,34 +1,62 @@
-import React from 'react'
-import Nav from '../Nav/Nav'
-import './Landing.css'
-import macbook from './mockup2.png'
+import React from "react";
+import Nav from "../Nav/Nav";
+import "./Landing.css";
+import { Typewriter } from "react-simple-typewriter";
+import empMockup from './empMock.png'
 
 export default function Landing() {
   return (
-    <div className='Landing'>
-        <Nav />
-        <section className='one'>
-            <div className='front-page'>
-              <div className='title-textbox'>
-                <div className='title-text'>
-                   <h1>Manage Cdl</h1>
-                   <h2>The helper for your company</h2>
-                   <p>Easily manage and organize your bus company's information. This includes personal details, employees, vans, students, pay rates, contact information, and more.</p>
-                </div>
-                <div className='join-now'>
-                  Join Now For Free!
-                </div>
-              </div>
-              <div className='title-imagebox'>
-                    <img src={macbook} alt="" className='macbook' />
-              </div>
+    <div className="Landing">
+      <Nav />
+      <section className="one">
+        <div className="front-page">
+          <div className="title-textbox">
+            <div className="title-text">
+              <h2
+              // style={{
+              //   paddingTop: "5rem",
+              //   margin: "auto 0",
+              //   fontWeight: "normal",
+              // }}
+              >
+                Manage your
+                <br />{" "}
+                <span style={{ color: "#377dff", fontWeight: "bold" }}>
+                  {/* Style will be inherited from the parent element */}
+                  <Typewriter
+                    words={[
+                      "Employees",
+                      "Vans",
+                      "Pupils",
+                      "Invoices",
+                      "Company",
+                    ]}
+                    loop={100}
+                    cursor
+                    cursorStyle="|"
+                    typeSpeed={100}
+                    deleteSpeed={50}
+                    delaySpeed={1500}
+                    // onLoopDone={handleDone}
+                    // onType={handleType}
+                  />
+                </span>
+              </h2>
+              <p>
+                Easily manage and organize your bus company's information. This
+                includes personal details, pay rates,
+                contact information, and more.
+              </p>
             </div>
-          <div className="wave">
-    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-        <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" class="shape-fill"></path>
-    </svg>
-</div>
-        </section>
+            <div className="join-now">Join Now For Free!</div>
+          </div>
+          <div className="title-imagebox">
+            <div className="empMock-container">
+              <img src={empMockup} alt="" className="empMock" />
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
-  )
+  );
 }
