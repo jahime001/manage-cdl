faa39399971d84752968ae8a17114ba86301a75c
jahime001 <jahimecamea@gmail.com>
2023-01-18 17:01:06 -0500

    changelogs

--- src/Components/ChangeLogs/ChangeLogs.jsx
+++ src/Components/ChangeLogs/ChangeLogs.jsx
@@ -2,6 +2,36 @@ import React from 'react'
 
 export default function ChangeLogs() {
   return (
-    <div>ChangeLogs</div>
-  )
+    <div>
+      <h1>Recent Changes</h1>
+      <ul>
+        <li>Added a disabled state for buttons in sign up and sign in page.</li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+
+      <h1>Future Changes</h1>
+      <ul>
+        <li>Fix delete function for employees.</li>
+        <li>Work on vans commponent.</li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+
+      <h1>Known Bugs</h1>
+      <ul>
+        <li>There's no loading when creating a employee.</li>
+        <li>
+          Emplooyee profile pictures requires a reload to show up after
+          adding/changing it.
+        </li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
 }
